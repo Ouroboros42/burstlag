dd166a070ad3c0fa6d9ffc0d34f9f6b093676a1c
Ouroboros42 <lucas.gathercole@gmail.com>
2024-07-05 12:12:48 +0100

    Implement compiler-specific args (needs testing on other platforms)

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
 from Cython.Build import cythonize
+
 import numpy
 
 py_lang_level = "3"
 c_lang = "c++"
+c_stdlib_ver = "c++17"
 
 build_root = "build/"
 
 src_root = "src/"
-py_root = src_root + "burstlag/"
-cpp_root = py_root + "cpp/"
+package_root = src_root + "burstlag/"
+cpp_root = package_root + "cpp/"
 
-cpp_lib_root = "cpplib/"
+# 3rd party c++ libraries (header-only)
+cpp_lib_root = "cpplib/" 
 
 source_files = [
-    py_root + "__init__.pyx",
+    package_root + "__init__.pyx",
     cpp_root + "caching/factorials.cpp",
     cpp_root + "fast_sum/converging.cpp",
     cpp_root + "fast_sum/lazy.cpp",
@@ -22,14 +27,31 @@ source_files = [
     cpp_root + "inputs/relation.cpp"
 ]
 
+# Locations with header files
 include_dirs = [
     cpp_lib_root,
     cpp_root,
     numpy.get_include()
 ]
 
-ext = Extension("*", source_files, language=c_lang, extra_compile_args = [ "/std:c++17" ])
+# Match compiler-dependent argument format
+platform_extra_compile_args = {
+    "msvc": [ f"/std:{c_stdlib_ver}" ],
+    "unix": [ f"-std={c_stdlib_ver}"]
+}
 
-cython_module = cythonize(ext, build_dir=build_root, language_level=py_lang_level)
+class PlatformSpecificBuildExt(build_ext):
+    def build_extensions(self) -> None:
+        extra_compile_args = platform_extra_compile_args.get(self.compiler.compiler_type, [])
 
-setup(name='burst-lag', ext_modules=cython_module, include_dirs=include_dirs)
+        for ext in self.extensions:
+            ext.extra_compile_args = extra_compile_args
+
+        return super().build_extensions()
+
+# Object representing extension module
+extension = Extension("*", source_files, language=c_lang)
+cython_module = cythonize(extension, build_dir=build_root, language_level=py_lang_level)
+
+# Build package
+setup(name='burst-lag', ext_modules=cython_module, include_dirs=include_dirs, cmdclass={"build_ext": PlatformSpecificBuildExt})
