a61e7c86e380c096e60addf125287896cfb016e5
Ouroboros42 <lucas.gathercole@gmail.com>
2024-07-10 14:48:06 +0100

    Make max_lag a parameter of mesh

--- src/burstlag/findpeak/fillmesh.py
+++ src/burstlag/findpeak/fillmesh.py
@@ -6,13 +6,13 @@ from typing import Callable
 
 MAX_LAG = 0.05
 
-def mesh(n_points: int):
-    time_diffs = np.linspace(-MAX_LAG, MAX_LAG, n_points)
+def mesh(n_points: int, max_lag: float = MAX_LAG):
+    time_diffs = np.linspace(-max_lag, max_lag, n_points)
 
-def likelihood_mesh(detectors: DetectorRelation, counts_1: np.ndarray, get_offset_counts_2: Callable[[float], np.ndarray], n_points: int) -> tuple[np.ndarray[float], np.ndarray[float]]:
+def likelihood_mesh(detectors: DetectorRelation, counts_1: np.ndarray, get_offset_counts_2: Callable[[float], np.ndarray], n_points: int, max_lag: float = MAX_LAG) -> tuple[np.ndarray[float], np.ndarray[float]]:
     cache = FactorialCache()
 
-    t_mesh = mesh(n_points)
+    t_mesh = mesh(n_points, max_lag)
 
     likelihoods = np.empty_like(t_mesh)
 
