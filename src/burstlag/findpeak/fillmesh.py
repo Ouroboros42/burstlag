23ba1e37e6d3f99cfce0795b3b6a5e98da4a5efc
Ouroboros42 <lucas.gathercole@gmail.com>
2024-07-12 15:18:10 +0100

    Fix import, missing argument, missing return

--- src/burstlag/findpeak/fillmesh.py
+++ src/burstlag/findpeak/fillmesh.py
@@ -7,9 +7,9 @@ from typing import Callable
 MAX_LAG = 0.05
 
 def mesh(n_points: int, max_lag: float = MAX_LAG):
-    time_diffs = np.linspace(-max_lag, max_lag, n_points)
+    return np.linspace(-max_lag, max_lag, n_points)
 
-def likelihood_mesh(detectors: DetectorRelation, counts_1: np.ndarray, get_offset_counts_2: Callable[[float], np.ndarray], n_points: int, max_lag: float = MAX_LAG) -> tuple[np.ndarray[float], np.ndarray[float]]:
+def likelihood_mesh(detectors: DetectorRelation, rel_accuracy: float, counts_1: np.ndarray, get_offset_counts_2: Callable[[float], np.ndarray], n_points: int, max_lag: float = MAX_LAG) -> tuple[np.ndarray[float], np.ndarray[float]]:
     cache = FactorialCache()
 
     t_mesh = mesh(n_points, max_lag)
@@ -17,6 +17,6 @@ def likelihood_mesh(detectors: DetectorRelation, counts_1: np.ndarray, get_offse
     likelihoods = np.empty_like(t_mesh)
 
     for i, time_diff in enumerate(t_mesh):
-        likelihoods[i] = log_likelihood(cache, detectors, counts_1, get_offset_counts_2(time_diff))
+        likelihoods[i] = log_likelihood(cache, detectors, counts_1, get_offset_counts_2(time_diff), rel_accuracy)
 
     return t_mesh, likelihoods
